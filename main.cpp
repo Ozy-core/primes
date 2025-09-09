15516d742e376d64cbaf0f973d556e0b8a7e1fc6
Osier Nono <osiernono@gmail.com>
2025-09-09 13:12:41 -0500

    setting up my different cpp and hpp's

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "prime_num.hpp"
+
+int main()
+{
+    int num;
+    cout<< "Enter a number to see if it's prime: ";
+    cin>>num;
+    primes(num);
+
+    return 0;
+}
---
+++ prime_num.cpp
@@ -0,0 +1,14 @@
+
+#include "prime_num.hpp"
+
+int primes(int i)
+ {
+    vector <int> prime_list;
+
+    if (i>1)
+    {
+        
+    }
+ }
+
+
